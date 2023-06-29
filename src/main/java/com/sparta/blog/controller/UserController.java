791144afcecf47b40a2cad650d3cbb4668388300
ChuJH <09strawberry@naver.com>
2023-06-29 17:40:42 +0900

    +Login Security

--- src/main/java/com/sparta/blog/controller/UserController.java
+++ src/main/java/com/sparta/blog/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.sparta.blog.controller;
 
-import com.sparta.blog.dto.LoginRequestDto;
-import com.sparta.blog.dto.RequestDto;
 import com.sparta.blog.dto.SignupRequestDto;
 import com.sparta.blog.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -26,27 +25,17 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public void signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
+    public void signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
         // Validation 예외처리
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         if(fieldErrors.size() > 0) {
             for (FieldError fieldError : bindingResult.getFieldErrors()) {
                 log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
             }
+            return;
         }
 
         userService.signup(requestDto);
     }
 
-    @PostMapping("/user/login")
-    public String login(@RequestBody LoginRequestDto requestDto, HttpServletResponse res){
-        try {
-            userService.login(requestDto, res);
-        } catch (Exception e) {
-            return "로그인 실패";
-        }
-
-        return "로그인 성공, 상태: " + res.getStatus();
-
-    }
 }
