b041916242e873ba0272d664951c04733bf7eab4
ChuJH <09strawberry@naver.com>
2023-06-29 14:29:45 +0900

    Filter

---
+++ src/main/java/com/sparta/blog/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.sparta.blog.controller;
+
+import com.sparta.blog.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @GetMapping("/products")
+    public String getProducts(HttpServletRequest req) {
+        System.out.println("ProductController.getProducts : 인증 완료");
+        User user = (User) req.getAttribute("user");
+
+
+        return "user.getUsername() = 하기싫다." + user.getUsername();
+    }
+}
