fcabbc27ebc9f95fe89499b8b2d1f253d64a782f
ChuJH <09strawberry@naver.com>
2023-08-04 16:36:02 +0900

    lv4 + StatusCode400 예외처리

--- src/main/java/com/sparta/blog/user/controller/UserController.java
+++ src/main/java/com/sparta/blog/user/controller/UserController.java
@@ -2,18 +2,16 @@ package com.sparta.blog.user.controller;
 
 import com.sparta.blog.ApiResponseDto;
 import com.sparta.blog.security.dto.LoginRequestDto;
-import com.sparta.blog.user.dto.SignupRequestDto;
 import com.sparta.blog.security.jwt.JwtUtil;
+import com.sparta.blog.user.dto.SignupRequestDto;
 import com.sparta.blog.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -26,18 +24,31 @@ public class UserController {
     @PostMapping("/user/signup")
     public ResponseEntity<ApiResponseDto> signup(@RequestBody @Valid SignupRequestDto requestDto) {
 
-        userService.signup(requestDto);
+        try{
+            userService.signup(requestDto);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(new ApiResponseDto("중복된 username입니다.", HttpStatus.BAD_REQUEST.value()));
+        }
 
         return ResponseEntity.status(201).body(new ApiResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
     }
 
     @PostMapping("/user/login")
     public ResponseEntity<ApiResponseDto> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response){
-        userService.login(requestDto);
-
+        try{
+            userService.login(requestDto);
+        }catch(IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(new ApiResponseDto("회원을 찾을 수 없습니다.", HttpStatus.BAD_REQUEST.value()));
+        }
         // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
         response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(requestDto.getUsername(), requestDto.getRole()));
 
         return ResponseEntity.ok().body(new ApiResponseDto("로그인 성공", HttpStatus.CREATED.value()));
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ApiResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        ApiResponseDto restApiException = new ApiResponseDto(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST.value());
+        return new ResponseEntity<>(restApiException, HttpStatus.BAD_REQUEST);
     }
 }
